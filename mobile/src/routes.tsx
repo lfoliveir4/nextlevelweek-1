c5db840e0696a0eee38344aa13181bfe366cbf5a
Luis Filipe Alves de Oliveira <macbook-pro-lf-oliveira@MacBook-Pro-de-Luis.local>
2020-10-17 02:49:17 -0300

    First Commit in new Repo

---
+++ mobile/src/routes.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
+
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+import Points from "./pages/Points";
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        headerMode="none"
+        screenOptions={{
+          cardStyle: {
+            backgroundColor: "#f0f0f5",
+          },
+        }}
+      >
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Points" component={Points} />
+        <Stack.Screen name="Detail" component={Detail} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
